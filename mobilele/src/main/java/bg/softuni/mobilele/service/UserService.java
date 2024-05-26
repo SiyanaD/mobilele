bf581b2ac812a4e875e7669017703d9db9c319d1
Siyana Danchilova <120177143+SiyanaD@users.noreply.github.com>
2024-05-26 23:07:57 +0300

    update

---
+++ mobilele/src/main/java/bg/softuni/mobilele/service/UserService.java
@@ -0,0 +1,7 @@
+package bg.softuni.mobilele.service;
+
+import bg.softuni.mobilele.model.UserRegistrationDTO;
+
+public interface UserService {
+    void registerUser(UserRegistrationDTO userRegistrationDTO);
+}
